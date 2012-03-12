be8f345f2ba947783b9f4ab8f7d8cbf69222db4b
PeeHaa <info@pieterhordijk.com>
2012-03-13 00:18:51 +0100

    Implemented support for animations Improved defaults

--- jquery-chariterator-0.1.js
+++ jquery-chariterator-0.1.js
@@ -2,19 +2,23 @@
  * jQuery charIterator plugin
  * Loops through all characters in a string in a element and optionally applies an animation (or function) to it
  *
- * jQuery 1.7
+ * jQuery 1.7 (jQuery UI 1.8 is needed optionally for animations)
  *
  * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @website    https://github.com/PeeHaa/charIterator-jQuery-plugin
  * @copyright  Copyright (c) 2012 Pieter Hordijk
  * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
  * @version    1.0.0
  */
 (function($) {
-  $.fn.CharIterator = function(options) {
+  $.fn.charIterator = function(options) {
     var defaults = {
-      skipSpaces: false,
-      insertBefore: '<span>',
-      insertAfter: '</span>'
+      skipSpaces: true,
+      insertBefore: '',
+      insertAfter: '',
+      animation: function() { },
+      loop: true,
+      timeout: 200
     };
 
     var opts = $.extend({}, defaults, options);
@@ -30,16 +34,35 @@
         } else if(this.nodeType === 3) {
           $(this).replaceWith($.map(this.nodeValue.split(''), function(c) {
             if (trim(c) != '' || !opts.skipSpaces) {
-              return opts.insertBefore + c + opts.insertAfter;
+              return opts.insertBefore + '<span class="charIterator-wrap">' + c + '</span>' + opts.insertAfter;
             }
             return c;
           }).join(''));
         }
       });
-    }
+    };
+
+    var animate = function(elem) {
+      var timeTillNextIteration = opts.timeout;
+      $('.charIterator-wrap', elem).each(function() {
+        var theElem = this;
+        setTimeout(function(){
+          opts.animation(theElem);
+        }, timeTillNextIteration)
+        timeTillNextIteration += opts.timeout;
+      });
+
+      if (opts.loop === true || (opts.loop !== false && opts.loop > 1)) {
+        if (opts.loop !== true) opts.loop = opts.loop - 1;
+        setTimeout(function(){
+          animate(elem);
+        }, timeTillNextIteration);
+      }
+    };
 
     return this.each(function() {
       wrap(this);
+      animate(this);
     });
   };
 })(jQuery);
